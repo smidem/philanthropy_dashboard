29d262e21f38ff5e93a977c10487d950440045b3
Shane McGuckian <shane.mcguckian@gmail.com>
2020-10-29 11:21:15 -0400

    Added several KPIs into data table.

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,4 @@
-from dashboard.get_data import get_data
+from dashboard.get_data import data
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -9,6 +9,21 @@ server = app.server
 
 app.layout = html.Div([
     html.P(html.Div(html.H3('Annual Campaign Dashboard'))),
+    dash_table.DataTable(
+        id='philanthropy_dashboard',
+        style_table={
+            'maxHeight': '700px',
+            'overflowY': 'scroll'
+        },
+        columns=[
+            {'name': "", 'id': "labels"},
+            {'name': "FY21 YTD", 'id': "fy21_ytd"},
+            {'name': "FY21 Goal", 'id': "fy21_goal"},
+            # {'name': "% to Goal", 'id': "goal_pct"},
+            # {'name': "FY20 YTD", 'id': "fy20_ytd"},
+        ],
+        data=data.to_dict('records'),
+    )
 ])
 
 if __name__ == '__main__':
--- dashboard/get_data.py
+++ dashboard/get_data.py
@@ -7,13 +7,39 @@ def to_dataframe(headers: list, data: List[list], **kwargs):
     return pd.DataFrame(columns=headers, data=data, **kwargs)
 
 
-def get_data(name, db_id='4249COL', **kwargs):
+def get_kpi(name, db_id='4249COL'):
     query_id = get_query_id(name=name, db_id=db_id)
     results = run_query(query_id=query_id, db_id=db_id, max_rows=10000)
-    df = to_dataframe(*results, **kwargs)
+    df = to_dataframe(*results)
     df['Amount'] = df['Amount'].astype('float')
     return sum(df['Amount'].to_list())
 
 
+data_dict = {
+    'Labels': [
+        'Flagship',
+        'Israel/Global',
+        # 'Jewish Life and Engagement',
+        # 'Jewish Community Relations Council',
+        # 'Regional Security Initiative',
+        'Aspen',
+        'Sponsorships',
+        # 'Operations',
+    ],
+    'FY21 YTD': [
+        get_kpi('KPI Unrestricted - AK'),
+        get_kpi('KPI Israel Global - SM'),
+        get_kpi('KPI Aspen - AK'),
+        get_kpi('KPI In year sponsorships and grants - AK'),
+    ],
+    'FY21 Goal': [
+        '$3,500,000',
+        '$0',
+        '$0',
+        '$528,000',
+    ]
+}
+data = pd.DataFrame.from_dict(data_dict)
+
 if __name__ == '__main__':
-    print(get_data('KPI Rest to Jco (new way) - AK'))
+    df = pd.DataFrame.from_dict(data_dict)
