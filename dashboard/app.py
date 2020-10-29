d636977f4b4adbd86dea39cb08608179557815da
Shane McGuckian <shane.mcguckian@gmail.com>
2020-10-29 12:34:56 -0400

    Changing styling on data table.

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,35 +14,32 @@ app.layout = html.Div([
                 'e3acd8c9d23315eb/72be3792-bb15-4a5a-81c2-3eb9c7a04d8e/'
                 '241x51.png'
         ),
-        style={'textAlign': 'center'}
+        # style={'textAlign': 'center'}
     ),
     html.P(
         html.Div(
             html.H3('Annual Campaign Dashboard'),
-            style={'textAlign': 'center'}
+            # style={'textAlign': 'center'}
         )
     ),
-    html.Div(
-        dash_table.DataTable(
-            id='philanthropy_dashboard',
-            style_table={
-                'maxHeight': '700px',
-                'maxWidth': '700px',
-                # 'overflowY': 'scroll',
-            },
-            # style_cell={
-            #     'maxWidth': '50px',
-            # },
-            columns=[
-                {'name': "", 'id': "labels"},
-                {'name': "FY21 YTD", 'id': "fy21_ytd"},
-                {'name': "FY21 Goal", 'id': "fy21_goal"},
-                # {'name': "% to Goal", 'id': "goal_pct"},
-                # {'name': "FY20 YTD", 'id': "fy20_ytd"},
-            ],
-            data=data.to_dict('records'),
-        ),
-        style={'textAlign': 'center'},
+    dash_table.DataTable(
+        id='philanthropy_dashboard',
+        style_table={
+            'maxHeight': '700px',
+            'maxWidth': '700px',
+            # 'overflowY': 'scroll',
+        },
+        # style_cell={
+        #     'maxWidth': '50px',
+        # },
+        columns=[
+            {'name': "", 'id': "labels"},
+            {'name': "FY21 YTD", 'id': "fy21_ytd"},
+            {'name': "FY21 Goal", 'id': "fy21_goal"},
+            # {'name': "% to Goal", 'id': "goal_pct"},
+            # {'name': "FY20 YTD", 'id': "fy20_ytd"},
+        ],
+        data=data.to_dict('records'),
     ),
 ])
 
